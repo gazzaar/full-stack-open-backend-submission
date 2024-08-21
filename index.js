665cb847177186dd457be7cf84255f6d032ec860
gazzaar <gazzarDev@gmail.com>
2024-08-22 02:34:38 +0300

    exercise 3.1 phonebook backend step 1

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello manga');
+const express = require('express');
+let persons = require('./persons');
+const app = express();
+
+const route = '/api/persons/';
+
+app.get(route, (req, res) => {
+  res.json(persons);
+});
+
+app.get(`${route}:id`, (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((person) => person.id == id);
+  res.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`app starts at port${PORT} have fun!`);
+});
