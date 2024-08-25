9dd40efaa7a7c138490f6be917e06164e8ea3787
gazzaar <gazzarDev@gmail.com>
2024-08-25 03:57:31 +0300

    Use static to deploy static site

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
-
+app.use(express.static('dist'));
 const route = '/api/persons/';
 
 app.get(route, (_, res) => {
