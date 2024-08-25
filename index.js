e93971b6781d67047e161fd39207ebf819d7fd2a
gazzaar <gazzarDev@gmail.com>
2024-08-25 03:24:02 +0300

    edit port bc Paas

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post(route, (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`app starts at port ${PORT} have fun!`);
 });
